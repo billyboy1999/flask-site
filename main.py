47d62bc34e565b9aee993665a8124ccb659998e1
Will <willcthefourth@gmail.com>
2014-06-20 17:54:29 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template("base.html", name="Home", content="Blah, blah, blah!")
+
+@app.route('/games/')
+def games():
+    return render_template("base.html", name="Games", content="Testicles")
+
+@app.route('/french/')
+def french():
+    return render_template("base.html", name="French", content="Le Penis")
+
+if __name__ == '__main__':
+    app.run(debug=True)
